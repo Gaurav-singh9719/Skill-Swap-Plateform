8449ebf29b29275dfc1a3426839e38c68d37d0d2
Saumyasharma974 <sharmasaumya2464@gmail.com>
2025-07-12 13:50:40 +0530

    feedback  functionality added

--- backend/routes/swapRoutes.js
+++ backend/routes/swapRoutes.js
@@ -7,6 +7,7 @@ import {
   deleteSwap
 } from '../controllers/swapController.js';
 import protect from '../middleware/authMiddleware.js';
+import isAdmin from '../middleware/adminMiddleware.js';
 
 const router = express.Router();
 const upload = multer(); // no storage needed for no files
@@ -14,7 +15,7 @@ const upload = multer(); // no storage needed for no files
 // 👇 Add .none() to parse form-data fields
 router.post('/', protect, upload.none(), createSwap);
 router.get('/', protect, getMySwaps);
-router.put('/:id', protect, updateSwapStatus);
-router.delete('/:id', protect, deleteSwap);
+router.put('/:id', protect, isAdmin, updateSwapStatus);
+router.delete('/:id', protect, isAdmin, deleteSwap);
 
 export default router;
